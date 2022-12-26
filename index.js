e7a33ff8387e0ecc9bd852de39c958679e0eaa84
nicolas nicolas <nicolasmail209@gmail.com>
2022-12-25 23:06:33 -0300

    Se formatean todos los archivos con Prettier
    Asi es mas facil su lectura.

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const $empezar = document.querySelector("#empezar");
 const $ronda = document.querySelector("#ronda");
 const $perdiste = document.querySelector("#perdiste");
@@ -6,114 +5,100 @@ const $perdiste = document.querySelector("#perdiste");
 let ronda = 1;
 let secuencia;
 
-$empezar.onclick = function(){
+$empezar.onclick = function () {
+  ronda = 1;
+  $perdiste.innerText = "";
+  $ronda.innerText = ronda;
+  secuencia = crearSecuencia(ronda);
+  mostrarSecuencia();
+};
 
-    ronda = 1;
-    $perdiste.innerText = "";
-    $ronda.innerText = ronda;
-    secuencia = crearSecuencia(ronda);
-    mostrarSecuencia();
-}
+function encender(id) {
+  const $cuadrado = document.querySelector("#" + id);
 
+  $cuadrado.classList.add("encendido");
 
-
-function encender(id){
-    
-    const $cuadrado = document.querySelector("#" + id);
-
-    $cuadrado.classList.add("encendido");
-
-    setTimeout(() => {
-        $cuadrado.classList.remove("encendido");
-    }, 2000);
-
-    
+  setTimeout(() => {
+    $cuadrado.classList.remove("encendido");
+  }, 2000);
 }
 
 function obtenerEnteroAleatorioInclusivo(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min);
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function crearSecuencia(ronda){
-    
-    const secuencia = [];
+function crearSecuencia(ronda) {
+  const secuencia = [];
 
-    for(let i = 0; i < ronda; i++){
-        secuencia[i] = obtenerEnteroAleatorioInclusivo(1, 4);
-        if(secuencia[i] === 1){
-            secuencia[i] = "uno";
-        }
-        if(secuencia[i] === 2){
-            secuencia[i] = "dos";
-        }
-        if(secuencia[i] === 3){
-            secuencia[i] = "tres";
-        }
-        if(secuencia[i] === 4){
-            secuencia[i] = "cuatro";
-        }
+  for (let i = 0; i < ronda; i++) {
+    secuencia[i] = obtenerEnteroAleatorioInclusivo(1, 4);
+    if (secuencia[i] === 1) {
+      secuencia[i] = "uno";
     }
+    if (secuencia[i] === 2) {
+      secuencia[i] = "dos";
+    }
+    if (secuencia[i] === 3) {
+      secuencia[i] = "tres";
+    }
+    if (secuencia[i] === 4) {
+      secuencia[i] = "cuatro";
+    }
+  }
 
-    return secuencia;
+  return secuencia;
 }
-
 
 TIEMPO_ENTRE_ENCENDIDOS = 3000;
 
-function mostrarSecuencia(){
-var counter = 0;
-var interval = setInterval(function(){
-   encender(secuencia[counter]);
-   counter+=1;
-   if (counter == (secuencia.length)) { clearInterval(interval); }
-}, TIEMPO_ENTRE_ENCENDIDOS);
+function mostrarSecuencia() {
+  var counter = 0;
+  var interval = setInterval(function () {
+    encender(secuencia[counter]);
+    counter += 1;
+    if (counter == secuencia.length) {
+      clearInterval(interval);
+    }
+  }, TIEMPO_ENTRE_ENCENDIDOS);
 }
 
+let numeroDeClick = 0;
 
+const $cuadrado1 = document.querySelector("#uno");
+const $cuadrado2 = document.querySelector("#dos");
+const $cuadrado3 = document.querySelector("#tres");
+const $cuadrado4 = document.querySelector("#cuatro");
 
-    let numeroDeClick = 0;
+function compararClickConSecuencia(cuadrado) {
+  encender(cuadrado.id);
+  if (!(cuadrado.id === secuencia[numeroDeClick])) {
+    $perdiste.innerText = "Perdiste";
+    numeroDeClick = 0;
 
-    
+    return;
+  }
+  numeroDeClick++;
+  if (numeroDeClick === ronda) {
+    numeroDeClick = 0;
+    ronda++;
+    $ronda.innerText = ronda;
+    secuencia = crearSecuencia(ronda);
+    mostrarSecuencia();
+    return;
+  }
+}
 
-    const $cuadrado1 = document.querySelector("#uno");
-    const $cuadrado2 = document.querySelector("#dos");
-    const $cuadrado3 = document.querySelector("#tres");
-    const $cuadrado4 = document.querySelector("#cuatro");
-
-    function compararClickConSecuencia(cuadrado){
-        encender(cuadrado.id);
-        if(!(cuadrado.id === secuencia[numeroDeClick])){
-            $perdiste.innerText = "Perdiste";
-            numeroDeClick = 0;
-            
-            return; 
-        }
-        numeroDeClick++;
-        if(numeroDeClick === ronda){
-            numeroDeClick = 0;
-            ronda++;
-            $ronda.innerText = ronda;
-            secuencia = crearSecuencia(ronda);
-            mostrarSecuencia();
-            return;
-        }
-    }
-
-    $cuadrado1.onclick = function(){
-        compararClickConSecuencia(this);
-    }
-    $cuadrado2.onclick = function(){
-        compararClickConSecuencia(this);
-    }
-    $cuadrado3.onclick = function(){
-        compararClickConSecuencia(this);
-    }
-    $cuadrado4.onclick = function(){
-        compararClickConSecuencia(this);
-    }
-
-
-
-
+$cuadrado1.onclick = function () {
+  compararClickConSecuencia(this);
+};
+$cuadrado2.onclick = function () {
+  compararClickConSecuencia(this);
+};
+$cuadrado3.onclick = function () {
+  compararClickConSecuencia(this);
+};
+$cuadrado4.onclick = function () {
+  compararClickConSecuencia(this);
+};
